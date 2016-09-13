8126908476d6288d0b6157d10e367d82603bedf2
Andy Halleran <halleran@caltech.edu>
2016-09-13 11:30:54 -0700

    bugfix on l20.py

--- scripts/l20.py
+++ scripts/l20.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+xa_high = np.loadtxt('../data/xa_high_food.csv')
+xa_low = np.loadtxt('data/xa_low_food.csv')
+
+def xa_to_diameter(xa):
+    """Convert xa into diameters"""
+
+    diameter = np.swrt(xa * 4 / np.pi)
+
+    return diameter
